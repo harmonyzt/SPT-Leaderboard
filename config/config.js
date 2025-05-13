8d057fb6a556339f092d950e771539825070666d
John Doe <hattrix@yandex.ru>
2025-05-13 09:59:17 +0500

    Add weapon mastery, health to entries

--- config/config.js
+++ config/config.js
@@ -1,13 +1,13 @@
 module.exports = {
     // If you want to share more Profile SPT stats with anyone and the leaderboard - set to true
-    public_profile: false,
+    public_profile: true,
 
     // If you want to display other name rather than your SPT profile name, put it here, or leave blank
-    // NOTE: Once you change your name and submit your stats - you won't be able to change it for 10 days!
+    // Maximum - 15 characters
     profile_customName: "",
 
     // Your about me to show for public profile. Maximum - 80 characters
-    profile_aboutMe: "I didn't read configuration and enabled public profile!",
+    profile_aboutMe: "1 like = 1 meow",
 
     // Profile picture
     // Optimal aspect ratio: 1:1 (500x500, 250x250 etc)
@@ -18,23 +18,23 @@ module.exports = {
     // Profile theme
     // Will change the background of your profile if it's public
     // Default | Dark | Light | Gradient
-    profile_profileTheme: "Default",
+    profile_profileTheme: "Gradient",
 
     // Prestige Styling (Public Profile only)
-    // If you have prestige unlocked - this can allow your profile name to change its style
+    // If you have prestige unlocked - this allows your profile name to change its style
     profile_usePrestigeStyling: true,
 
-    // Maximum raids to retry to connect to the leaderboard before pausing the mod
+    // Maximum raids to retry to connect to the leaderboard if it failed for first time
     connectionRetries: 1,
 
-    // Debug
+    // Display debug messages in console and log them inside SPT server .log file
     DEBUG: true,
 
     // DO NOT TOUCH UNLESS YOU KNOW WHAT YOU ARE DOING.
     // Domain (or both subdomain + domain) used for PHP requests
     PHP_ENDPOINT: "visuals.nullcore.net",
 
-    // Path to PHP file that proccess data incoming from SPT mod
-    // Example - domain.com/spt_profile_server.php <-- then put "/spt_profile_server.php"
+    // Path to PHP file that process incoming data from this mod
+    // Example - domain.com/spt_profile_server.php <-- in this case you put "/spt_profile_server.php"
     PHP_PATH: "/hidden/SPT_Profiles_Backend.php"
 };
--- src/mod.js
+++ src/mod.js
@@ -21,10 +21,13 @@ class SPTLeaderboard {
         this.staticProfile;
         this.serverMods;
         this.transitionMap;
+        this.lastRaidMap;
         this.mostRecentAchievementTimestamp = 0;
         this.mostRecentAchievementImageUrl = null;
         this.mostRecentAchievementName = null;
         this.mostRecentAchievementDescription = null;
+        this.latestWeaponId = null;
+        this.latestProgress = 0;
     }
 
     loadOrCreateToken() {
@@ -53,8 +56,8 @@ class SPTLeaderboard {
         const profileHelper = container.resolve("ProfileHelper");
         const config = this.CFG;
 
-        if(config.DEBUG){
-            logger.log("[SPT Leaderboard] Present Token: " + this.uniqueToken, "blue")
+        if (config.DEBUG) {
+            logger.log("[SPT Leaderboard RC] Present Token: " + this.uniqueToken, "blue")
         }
 
         logger.log(" ", "cyan");
@@ -63,13 +66,13 @@ class SPTLeaderboard {
         logger.log(" _\\/\\\\\\_____________\\/\\\\\\///////\\\\\\__       ", "cyan");
         logger.log("  _\\/\\\\\\_____________\\/\\\\\\_______\\/\\\\\\__      ", "cyan");
         logger.log("   _\\/\\\\\\_____________\\/\\\\\\\\\\\\\\\\\\\\\\\\\\\\__     ", "cyan");
-        logger.log("    _\\/\\\\\\_____________\\/\\\\\\/////////\\\\\\__       RC BUILD v2.1.0", "cyan");
+        logger.log("    _\\/\\\\\\_____________\\/\\\\\\/////////\\\\\\__       RC v2.2.0", "cyan");
         logger.log("     _\\/\\\\\\_____________\\/\\\\\\_______\\/\\\\\\__   ", "cyan");
         logger.log("      _\\/\\\\\\_____________\\/\\\\\\_______\\/\\\\\\__  ", "cyan");
         logger.log("       _\\/\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\_\\/\\\\\\\\\\\\\\\\\\\\\\/__ ", "cyan");
         logger.log("        _\\///////////////__\\/////////////_", "cyan");
         logger.log("=============================================", "cyan");
-        logger.info("[SPT Leaderboard] This is a test build from developer. Support provided only at Discord development channel!", "yellow");
+        logger.log("[SPT Leaderboard] This is a test build from developer. Only official Testers are allowed to participate. Support provided only at Discord development channel.", "yellow");
 
         function calculateFileHash(filePath) {
             const fileBuffer = fs.readFileSync(filePath);
@@ -122,6 +125,30 @@ class SPTLeaderboard {
         var coreConfig = configServer.getConfig("spt-core");
         var sptVersion = coreConfig.sptVersion;
 
+        // Find map name from serverId
+        function getPrettyMapName(entry) {
+            const mapAliases = {
+                "bigmap": "Customs",
+                "factory4_day": "Factory",
+                "factory4_night": "Night Factory",
+                "interchange": "Interchange",
+                "laboratory": "Labs",
+                "rezervbase": "Reserve",
+                "shoreline": "Shoreline",
+                "woods": "Woods",
+                "lighthouse": "Lighthouse",
+                "tarkovstreets": "Streets of Tarkov",
+                "sandbox": "Ground Zero - Low",
+                "sandbox_high": "Ground Zero - High"
+            };
+
+            const rawMapName = entry.split('.')[0];
+
+            rawMapName.toLowerCase();
+
+            return mapAliases[rawMapName] || rawMapName; // returning raw if not found
+        }
+
         RouterService.registerStaticRouter("SPTLBProfileRaidEnd", [{
             url: "/client/match/local/end",
             action: async (url, info, sessionId, output) => {
@@ -130,7 +157,7 @@ class SPTLeaderboard {
 
                 this.serverMods = this.staticProfile.spt.mods.map(mod => mod.name).join(', ');
 
-                if(config.DEBUG){
+                if (config.DEBUG) {
                     logger.info(`Server Mods:  ` + this.serverMods);
                 }
 
@@ -145,10 +172,10 @@ class SPTLeaderboard {
             action: async (url, info, sessionId, output) => {
                 this.staticProfile = profileHelper.getPmcProfile(sessionId);
                 const allAchievements = JSON.parse(output);
-        
+
                 let latestAchievement = null;
                 let latestTimestamp = 0;
-        
+
                 // Finding the most recent achivement
                 if (this.staticProfile?.Achievements) {
                     for (const [achievementId, timestamp] of Object.entries(this.staticProfile.Achievements)) {
@@ -158,29 +185,40 @@ class SPTLeaderboard {
                         }
                     }
                 }
-        
+
+                if (this.staticProfile?.Skills.Mastering) {
+                    for (const weapon of this.staticProfile.Skills.Mastering) {
+                        if (weapon.Progress > this.latestProgress) {
+                            this.latestProgress = weapon.Progress;
+                            this.latestWeaponId = weapon.Id;
+                        }
+                    }
+                }
+
+                logger.log(`${this.latestWeaponId}`, "cyan")
+
                 // Get the image URL and name/description from locale file
                 if (latestAchievement) {
                     let achievementImageUrl = "";
                     const achievementElements = allAchievements?.data?.elements || [];
                     const achievementData = achievementElements.find(el => el?.id === latestAchievement);
-                    
+
                     if (achievementData) {
                         achievementImageUrl = achievementData.imageUrl || "";
                     }
-        
+
                     const localePath = path.join(__dirname, "..", "temp", "locale.json");
                     let achievementName = "";
                     let achievementDescription = "";
-        
+
                     try {
                         const localeFileContent = fs.readFileSync(localePath, "utf-8");
                         const localeData = JSON.parse(localeFileContent);
-        
+
                         if (localeData) {
                             const nameKey = `${latestAchievement} name`;
                             const descKey = `${latestAchievement} description`;
-        
+
                             achievementName = localeData[nameKey] || "";
                             achievementDescription = localeData[descKey] || "";
                         }
@@ -192,16 +230,15 @@ class SPTLeaderboard {
                     this.mostRecentAchievementImageUrl = achievementImageUrl;
                     this.mostRecentAchievementName = achievementName;
                     this.mostRecentAchievementDescription = achievementDescription;
-        
+
                 } else {
-                    if(config.DEBUG)
+                    if (config.DEBUG)
                         logger.info("No achievements found in profile");
                 }
-        
+
                 return output;
             }
         }], "aki");
-        
 
         const gatherProfileInfo = async (data, logger, version) => {
 
@@ -213,10 +250,11 @@ class SPTLeaderboard {
                 logger.log("Data above was saved in server log file.", "green");
             }
 
+            this.lastRaidMap = getPrettyMapName(jsonData.serverId);
+
             // Get the result of a raid (Died/Survived/Runner)
             this.raidResult = jsonData.results.result;
             this.playTime = jsonData.results.playTime;
-
             this.transitionMap = jsonData?.locationTransit?.location ?? "None";
 
             try {
@@ -278,10 +316,19 @@ class SPTLeaderboard {
             const config = this.CFG;
             const isScavRaid = profile.Info.Side === "Savage";
 
+            // Combine mods into one string
+            const combinedModData = modData + this.serverMods;
+
             // Keep names and such separate
             let scavLevel = this.staticProfile.characters.scav.Info.Level;
             let pmcLevel = this.staticProfile.characters.pmc.Info.Level;
-            let profileName = this.staticProfile.characters.pmc.Info.Nickname;
+            let profileName = "default_name";
+
+            if (config.public_profile && !!profileName?.trim()) {
+                profileName = config.profile_customName;
+            } else {
+                profileName = this.staticProfile.characters.pmc.Info.Nickname;
+            }
 
             // Initial Profile Stats that are always used
             const kills = getStatValue(['KilledPmc']);
@@ -310,10 +357,10 @@ class SPTLeaderboard {
             const curWinStreak = getGlobalStatValue(['CurrentWinStreak', 'Pmc']);
             const longestShot = getGlobalStatValue(['LongestKillShot']);
             const lootEXP = getStatValue(['ExpLooting']);
-            
+
             // Handling hits this way because stats wont be sent if there's 0 hits
             let lastHits = getStatValue(['HitCount']);
-            if(!lastHits || lastHits <= 0){
+            if (!lastHits || lastHits <= 0) {
                 lastHits = 0;
             }
 
@@ -323,14 +370,23 @@ class SPTLeaderboard {
             const totalLongestShot = parseInt(modLongestShot.slice(0, -2), 10);
             const totalDamage = parseInt(modDamage.slice(0, -2), 10);
 
+            // Get max PMC health
+            const totalMaxHealth = Object.values(this.staticProfile.Health.BodyParts)
+                .reduce((sum, bodyPart) => sum + (bodyPart.Health?.Maximum || 0), 0);
+
+            if(config.DEBUG){
+                console.info("Max Health Values (max 550):", totalMaxHealth);
+            }
+
             // Barebones of data
             const baseData = {
                 token: this.uniqueToken,
                 id: this.staticProfile.info.id,
                 modINT: this.key_size,
-                mods: modData,
+                mods: combinedModData,
                 pmcLevel: pmcLevel,
                 name: profileName,
+                health: totalMaxHealth,
                 raidKills: kills,
                 disqualified: false,
                 sptVer: versionSPT,
@@ -356,7 +412,7 @@ class SPTLeaderboard {
                     profilePicture: config.profile_profilePicture,
                     profileTheme: config.profile_profileTheme,
                     registrationDate: profile.Info.RegistrationDate,
-                    lastRaidMap: profile.Info.EntryPoint,
+                    lastRaidMap: this.lastRaidMap,
                     lastRaidEXP: lootEXP,
                     lastRaidHits: lastHits,
                     isTransition: isTransition,
@@ -367,7 +423,9 @@ class SPTLeaderboard {
                     latestAchievementName: this.mostRecentAchievementName,
                     latestAchievementDescription: this.mostRecentAchievementDescription,
                     latestAchievementImageUrl: this.mostRecentAchievementImageUrl,
-                    latestAchievementTimestamp: this.mostRecentAchievementTimestamp
+                    latestAchievementTimestamp: this.mostRecentAchievementTimestamp,
+                    weaponMasteryName: this.weaponMasteryName,
+                    weaponMasteryProgress: this.weaponMasteryProgress
                 }
                 // PMC Raid with public profile on
             } else if (config.public_profile && !isScavRaid) {
@@ -384,7 +442,7 @@ class SPTLeaderboard {
                     profilePicture: config.profile_profilePicture,
                     profileTheme: config.profile_profileTheme,
                     registrationDate: profile.Info.RegistrationDate,
-                    lastRaidMap: profile.Info.EntryPoint,
+                    lastRaidMap: this.lastRaidMap,
                     lastRaidEXP: lootEXP,
                     lastRaidHits: lastHits,
                     isTransition: isTransition,
@@ -395,7 +453,9 @@ class SPTLeaderboard {
                     latestAchievementName: this.mostRecentAchievementName,
                     latestAchievementDescription: this.mostRecentAchievementDescription,
                     latestAchievementImageUrl: this.mostRecentAchievementImageUrl,
-                    latestAchievementTimestamp: this.mostRecentAchievementTimestamp
+                    latestAchievementTimestamp: this.mostRecentAchievementTimestamp,
+                    weaponMasteryName: this.weaponMasteryName,
+                    weaponMasteryProgress: this.weaponMasteryProgress
                 }
                 // Private profile raid
             } else {
