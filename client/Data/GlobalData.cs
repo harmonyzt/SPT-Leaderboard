4410e2fc1187fc43a09395c83cd8ae7964b535ca
Kat <katrinfox0522@gmail.com>
2025-07-11 00:18:36 +0300

    Revert "Version exactly right"
    This reverts commit 4e5107fb16c0d2365f70ee4f638c037fbc9e1e70.
    
    # Conflicts:
    #	client/Data/GlobalData.cs

--- client/Data/GlobalData.cs
+++ client/Data/GlobalData.cs
@@ -5,7 +5,7 @@ namespace SPTLeaderboard.Data;
 
 public class GlobalData
 {
-    public static string Version = "3.1.0";
+    public static string Version = "2.6.0";
     public static string BaseSPTVersion = "3.11.3";
     public static string HeartbeatUrl = $"https://{SettingsModel.Instance.PhpEndpoint.Value}{SettingsModel.Instance.PhpPath.Value}heartbeat/v1.php";
     public static string ProfileUrl = $"https://{SettingsModel.Instance.PhpEndpoint.Value}{SettingsModel.Instance.PhpPath.Value}v2/v2.php";
