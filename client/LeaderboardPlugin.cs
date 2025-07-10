b7937fb8454c7ec0ad15d6689e5e45d3de617f33
Kat <katrinfox0522@gmail.com>
2025-07-10 18:08:33 +0300

    Changed: Add postBuildEvent & increase ver

--- client/LeaderboardPlugin.cs
+++ client/LeaderboardPlugin.cs
@@ -16,7 +16,7 @@ using SPTLeaderboard.Utils;
 
 namespace SPTLeaderboard
 {
-    [BepInPlugin("harmonyzt.SPTLeaderboard", "SPTLeaderboard", "0.1.1")]
+    [BepInPlugin("harmonyzt.SPTLeaderboard", "SPTLeaderboard", "3.1.0")]
     public class LeaderboardPlugin : BaseUnityPlugin
     {
         public static LeaderboardPlugin Instance { get; private set; }
