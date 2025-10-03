617345dd1b02bf45a6705a5db7a315beafa8c914
Yui <yui@osufx.com>
2025-10-03 16:13:12 -0300

    refactor(inbox): separated inbox check to standalone file

--- server/src/callbacks/LeaderboardItemCallbacks.ts
+++ server/src/callbacks/LeaderboardItemCallbacks.ts
@@ -1,10 +1,10 @@
 import { inject, injectable } from "tsyringe";
-import { ItemHelper } from "../helpers/ItemHelper";
+import { LeaderboardItemHelper } from "../helpers/ItemHelper";
 import { ILeaderboardGetItemPricesRequest } from "../models/routes/items/ILeaderboardGetItemPricesRequest";
 
 @injectable()
 export class LeaderboardItemCallbacks {
-    constructor(@inject("ItemHelper") protected itemHelper: ItemHelper) { }
+    constructor(@inject("LeaderboardItemHelper") protected itemHelper: LeaderboardItemHelper) { }
 
     public handleItemPrices(_url: string, _info: ILeaderboardGetItemPricesRequest, _sessionId: string): number {
         return this.itemHelper.getTotalFleaPrice(_info.templateIds);
--- server/src/helpers/ItemHelper.ts
+++ server/src/helpers/ItemHelper.ts
@@ -1,9 +1,16 @@
 import { inject, injectable } from "tsyringe";
 import { RagfairHelper } from "./RagfairHelper";
+import { ItemHelper } from "@spt/helpers/ItemHelper";
+import { HashUtil } from "@spt/utils/HashUtil";
+import { IItem } from "@spt/models/eft/common/tables/IItem";
 
 @injectable()
-export class ItemHelper {
-    constructor(@inject("RagfairHelper") protected ragfairHelper: RagfairHelper) { }
+export class LeaderboardItemHelper {
+    constructor(
+        @inject("RagfairHelper") protected ragfairHelper: RagfairHelper,
+        @inject("ItemHelper") protected itemHelper: ItemHelper,
+        @inject("HashUtil") protected hashUtil: HashUtil
+    ) { }
 
     public getTotalFleaPrice(templateIds: string[]): number {
         if (templateIds.length == 0) {
@@ -16,5 +23,14 @@ export class ItemHelper {
             totalPrice += lowestPrice;
         }
         return totalPrice;
+    }
+
+    public getItemInstancesAsFiR(templateIds: string[]): IItem[] {
+        let result: IItem[];
+        for (let item of templateIds) {
+            result.push({ _tpl: item, _id: this.hashUtil.generate() });
+        }
+        this.itemHelper.setFoundInRaid(result);
+        return result;
     }
 }
